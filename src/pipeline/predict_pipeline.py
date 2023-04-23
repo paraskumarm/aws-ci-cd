48c3c8a54f18531296c51579840ca683eef0b55b
paraskumarm <77846161+paraskumarm@users.noreply.github.com>
2023-04-23 17:34:49 +0530

    Update predict_pipeline.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -15,6 +15,7 @@ class PredictPipeline:
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
+            
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
